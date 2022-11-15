95217ad8afeb089cf4e94de6bf29a5be394d87c3
Shaikzaheer174 <shaikzaheer174@gmail.com>
2022-11-15 17:55:07 +0530

    parammap observables

--- helloworld/src/app/course-details/course-details.component.ts
+++ helloworld/src/app/course-details/course-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
@@ -10,11 +10,29 @@ export class CourseDetailsComponent implements OnInit {
 
   public courseId:any;
 
-  constructor(private _activatedRoute: ActivatedRoute) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _route: Router) { }
 
   ngOnInit() {
-   let cId = this._activatedRoute.snapshot.paramMap.get('id');
-   this.courseId = cId;
+  //  let cId = this._activatedRoute.snapshot.paramMap.get('id');
+
+   this._activatedRoute.paramMap.subscribe((params: ParamMap) => {  //paramMap returns observable so to capture we need subscribe()
+    let id = Number(params.get('id'));
+    this.courseId = id;
+   })
+   
   }
+
+  toPrevious(){
+    let previousId = this.courseId - 1;
+    this._route.navigate(['/course/', previousId]);
+
+
+  }
+
+  toNext(){
+    let nextId = this.courseId + 1;
+    this._route.navigate(['/course', nextId]);
+  }
+
 
 }
